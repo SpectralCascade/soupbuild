d998d27354a98b3c86f33f72456587dd9ecb1735
Tim Lane <timlanesoftware@gmail.com>
2022-01-26 14:02:19 +0000

    Soup tasks can now run other soup tasks

--- soupbuild.py
+++ soupbuild.py
@@ -9,6 +9,26 @@ from types import SimpleNamespace
 MAJOR_VERSION = 1
 MINOR_VERSION = 0
 
+quiet = False
+start_time = 0
+
+def log(message):
+    if (not quiet):
+        log_always(message)
+    
+def log_always(message):
+    print(("[{:.3f}] ".format(time.time() - start_time)) + message)
+
+def format_vars(data, config, mode, platform, root):
+    data = data.format(
+        name=config["name"],
+        output=config["output"],
+        mode=mode, platform=platform,
+        root=root,
+        run_task=("python3 " + os.path.join(root, "soupbuild.py") + " --quiet --task-only ")
+    )
+    return data
+
 def format_config(config, d, platform, mode, root):
     for k, v in d.items():
         if isinstance(v, dict):
@@ -21,28 +41,31 @@ def format_config(config, d, platform, mode, root):
                     d[k][item] = format_config(config, d[k][item], platform, mode, root)
                 elif isinstance(v[item], str):
                     #print("Formatting value of key " + k + ", list element " + str(item))
-                    d[k][item] = d[k][item].replace("{name}", config["name"])
-                    d[k][item] = d[k][item].replace("{output}", config["output"])
-                    d[k][item] = d[k][item].replace("{mode}", mode)
-                    d[k][item] = d[k][item].replace("{platform}", platform)
-                    d[k][item] = d[k][item].replace("{root}", root)
+                    d[k][item] = format_vars(d[k][item], config, mode, platform, root)
         elif isinstance(v, str):
             #print("Formatting value of key " + k)
-            d[k] = d[k].replace("{name}", config["name"])
-            d[k] = d[k].replace("{output}", config["output"])
-            d[k] = d[k].replace("{mode}", mode)
-            d[k] = d[k].replace("{platform}", platform)
-            d[k] = d[k].replace("{root}", root)
+            d[k] = format_vars(d[k], config, mode, platform, root)
     return d
 
 def execute(command):
-    print("> " + command)
+    log("$ " + command)
     return os.system(command)
 
 # Standard execution (in directory with a .soup file):
 # python3 soupbuild.py [platform] task [mode]
 if __name__ == "__main__":
-    print("Soupbuilder version " + str(MAJOR_VERSION) + "." + str(MINOR_VERSION))
+    start_time = time.time()
+    # Get command flags
+    quiet = "--quiet" in sys.argv
+    task_only = "--task-only" in sys.argv
+    
+    if (not quiet):
+        version_str = str(MAJOR_VERSION) + "." + str(MINOR_VERSION)
+        border = "////////////////" + ("/" * len(version_str))
+        print(border)
+        print("/ SOUPBUILDER " + version_str + " /")
+        print(border + "\n")
+
     cwd = os.getcwd()
     argc = len(sys.argv)
     
@@ -53,11 +76,14 @@ if __name__ == "__main__":
                 config = json.loads(data.read())
             break
     if (config == None):
-        print("\nFailed to find configuration file in CWD \"" + cwd + "\", aborting soupbuild.")
+        log("Failed to find configuration file in CWD \"" + cwd + "\", aborting soupbuild.")
         sys.exit(-1)
     
-    
     argi = 1
+    
+    # Skip options and flags
+    while (argi < argc and sys.argv[argi].startswith("-")):
+        argi += 1
     
     # Get the platform target
     platform = config["default-platform"]
@@ -77,7 +103,7 @@ if __name__ == "__main__":
         mode = sys.argv[argi]
         argi += 1
     
-    print("\nSoupbuild running with task \"" + task + "\" for platform \"" + (platform if len(platform) > 0 else "[all platforms]") + "\" in mode \"" + (mode if len(mode) > 0 else "[none]") + "\".")
+    log("Soupbuild running with task \"" + task + "\" for platform \"" + (platform if len(platform) > 0 else "[all platforms]") + "\" in mode \"" + (mode if len(mode) > 0 else "[none]") + "\".")
     
     platforms = []
     if (len(platform) == 0):
@@ -89,41 +115,42 @@ if __name__ == "__main__":
     
     original_config = config.copy()
     for platform in platforms:
-        start_time = time.time()
-    
+        task_start_time = time.time()
+        
         # Format config
         format_config(config, config, platform, mode, cwd)
         
-        # First get the template project copied to the working directory
-        print("\nCopying template project...")
         src = config["platforms"][platform]["template"]["project"]
         dest = config["work"] + "/" + os.path.split(config["platforms"][platform]["template"]["project"])[-1]
-        execute("mkdir \"" + dest + "\"")
-        execute("copy \"" + src + "\" \"" + dest + "\"")
-        
-        # Now link source code and assets - more efficient than copying.
-        print("\nLinking source and asset directories...")
-        full_code_dest = config["platforms"][platform]["template"]["source"]
-        full_assets_dest = config["platforms"][platform]["template"]["assets"]
-        code_dest = "\"" + os.path.join(dest, os.path.split(full_code_dest)[0]) + "\""
-        assets_dest = "\"" + os.path.join(dest, os.path.split(full_assets_dest)[0]) + "\""
-        full_code_dest = "\"" + os.path.join(dest, full_code_dest) + "\""
-        full_assets_dest = "\"" + os.path.join(dest, full_assets_dest) + "\""
-        
-        execute("mkdir " + code_dest)
-        execute("mkdir " + assets_dest)
-        execute("mklink /J " + full_code_dest + " \"" + config["source"] + "\"")
-        execute("mklink /J " + full_assets_dest + " \"" + config["assets"] + "\"")
+        if (not task_only):
+            # First get the template project copied to the working directory
+            log("Copying template project...")
+            execute("mkdir \"" + dest + "\"")
+            execute("copy \"" + src + "\" \"" + dest + "\"")
+            
+            # Now link source code and assets - more efficient than copying.
+            log("Linking source and asset directories...")
+            full_code_dest = config["platforms"][platform]["template"]["source"]
+            full_assets_dest = config["platforms"][platform]["template"]["assets"]
+            code_dest = "\"" + os.path.join(dest, os.path.split(full_code_dest)[0]) + "\""
+            assets_dest = "\"" + os.path.join(dest, os.path.split(full_assets_dest)[0]) + "\""
+            full_code_dest = "\"" + os.path.join(dest, full_code_dest) + "\""
+            full_assets_dest = "\"" + os.path.join(dest, full_assets_dest) + "\""
+            
+            execute("mkdir " + code_dest)
+            execute("mkdir " + assets_dest)
+            execute("mklink /J " + full_code_dest + " \"" + config["source"] + "\"")
+            execute("mklink /J " + full_assets_dest + " \"" + config["assets"] + "\"")
         
         # Finally, execute the task steps in the working project directory
-        print("\nRunning task \"" + task + "\" for platform: " + platform)
+        log("Running task \"" + task + "\" for platform: " + platform)
         execute("cd \"" + dest + "\"")
         steps = config["platforms"][platform]["tasks"][task]
         num_steps = len(steps)
         
         failed = False
         for i in range(num_steps):
-            print("\nStep " + str(i + 1) + " of " + str(num_steps))
+            log("Step " + str(i + 1) + " of " + str(num_steps))
             if (execute(steps[i]) != 0):
                 failed = True
                 break
@@ -132,4 +159,7 @@ if __name__ == "__main__":
         execute("cd \"" + cwd + "\"")
         config = original_config.copy()
         
-        print("\n\nTask \"" + task + "\" " + ("FAILED" if failed else "SUCCEEDED") + " in " + ("{:.2f}".format(time.time() - start_time)) + " seconds for platform: " + platform)
+        log_always("Task \"" + task + "\" " + ("FAILED" if failed else "SUCCEEDED") + " in " + ("{:.2f}".format(time.time() - task_start_time)) + " seconds for platform: " + platform)
+        if (failed):
+            print("")
+            sys.exit(-1)
